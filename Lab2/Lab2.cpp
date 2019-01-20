88e126ca434ce5429891bc9badc490e5588bf4c0
rbcjf0219 <rbcjf0219@gmail.com>
2019-01-20 13:10:34 +0900

    실습2

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,78 @@
+#include "Lab2.h"
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+namespace lab2
+{
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int number;
+		string trash;
+
+		while (true)
+		{
+			in >> number;
+
+			if (in.eof())
+			{
+				in.clear();
+				break;
+			}
+
+			if (in.fail())
+			{
+				in.clear();
+				in >> trash;
+				continue;
+			}
+		
+			out << setfill(' ');
+			out << setw(12) << oct << number << ' ';
+			out << setw(10) << dec << number << ' ';
+			out << setw(8) << uppercase << hex << number;
+			out << endl;
+		}
+	}
+
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float number;
+		float max = -FLT_MAX;
+		string trash;
+
+		while (true)
+		{
+			in >> number;
+
+			if (in.eof())
+			{
+				in.clear();
+
+				out << setfill(' ');
+				out << setw(5) << left << "max:";
+				out << setw(15) << showpos << showpoint << internal << fixed << setprecision(3) << max;
+
+				break;
+			}
+
+			if (in.fail())
+			{
+				in.clear();
+				in >> trash;
+				continue;
+			}
+
+			if (max < number)
+			{
+				max = number;
+			}
+
+			out << setfill(' ');
+			out << setw(5) << "";
+			out << setw(15) << showpos << showpoint << internal << fixed << setprecision(3) << number;
+			out << endl;
+		}
+	}
+}
--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,4 +1,17 @@
+#include "Lab2.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace lab2;
+using namespace std;
+
 int main()
 {
+	cout << setw(12) << "oct" << ' ';
+	cout << setw(10) << "dec" << ' ';
+	cout << setw(8) << "hex" << endl;
+	cout << setw(12) << setfill('-') << "" << ' ' << setw(10) << "" << ' ' << setw(8) << "" << endl;
 
+	PrintIntegers(cin, cout);
+	PrintMaxFloat(cin, cout);
 }
