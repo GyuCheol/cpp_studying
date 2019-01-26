5827d7d55f06677864f3cbf7f23a15bd7998fd69
rbcjf0219 <rbcjf0219@gmail.com>
2019-01-26 22:14:37 +0900

    실습3 업로드 1회차

---
+++ Lab3/TimeSheet.cpp
@@ -0,0 +1,90 @@
+#include "Timesheet.h"
+#include <math.h>
+
+namespace lab3
+{
+	
+
+	TimeSheet::TimeSheet(const char* name, unsigned int maxEntries)
+		: mMaxEntries(maxEntries)
+		, mTimeTotal(0)
+		, mTimeIndex(0)
+		, mName(name)
+	{
+		mTimeArray = new int[maxEntries];
+	}
+
+	TimeSheet::TimeSheet(const TimeSheet& other)
+		: mMaxEntries(other.mMaxEntries)
+		, mTimeIndex(other.mTimeIndex)
+		, mTimeTotal(other.mTimeTotal)
+		, mName(other.GetName())
+	{
+		mTimeArray = new int[mMaxEntries];
+		memcpy(mTimeArray, other.mTimeArray, mMaxEntries * sizeof(int));
+	}
+
+	TimeSheet::~TimeSheet() {
+		delete[] mTimeArray;
+	}
+
+	void TimeSheet::AddTime(int timeInHours)
+	{
+		if (timeInHours < 1 || timeInHours > 10 || mTimeIndex == mMaxEntries)
+		{
+			return;
+		}
+
+		mTimeTotal += timeInHours;
+		mTimeArray[mTimeIndex] = timeInHours;
+		mTimeIndex++;
+	}
+
+	int TimeSheet::GetTimeEntry(unsigned int index) const
+	{
+		if (index >= mTimeIndex)
+		{
+			return -1;
+		}
+
+		return mTimeArray[index];
+	}
+
+	int TimeSheet::GetTotalTime() const
+	{
+		return mTimeTotal;
+	}
+
+	float TimeSheet::GetAverageTime() const
+	{
+		if (mTimeIndex == 0) {
+			return 0.0f;
+		}
+
+		return (float) mTimeTotal / mTimeIndex;
+	}
+
+	float TimeSheet::GetStandardDeviation() const
+	{
+		if (mTimeIndex == 0) {
+			return 0.0f;
+		}
+
+		float avg = GetAverageTime();
+		float total = .0f;
+
+		for (int i = 0; i < mTimeIndex; i++)
+		{
+			float diff = mTimeArray[i] - avg;
+
+			total += diff * diff;
+		}
+
+		return sqrt(total / mTimeIndex);
+	}
+
+	const std::string& TimeSheet::GetName() const
+	{
+		return mName;
+	}
+}
--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,3 +1,46 @@
-int main() {
+#include <iostream>
+#include "TimeSheet.h"
 
+using namespace std;
+using namespace lab3;
+
+class Vector
+{
+public:
+	int mX;
+	int mY;
+
+public:
+	Vector() 
+		: mX(0)
+		, mY(0)
+	{
+	}
+
+};
+
+Vector AddVector() {
+	Vector vector = Vector();
+
+	vector.mX = 10;
+	vector.mY = 10;
+
+	cout << &vector.mX << " " << &vector.mY << endl;
+
+	return vector;
+}
+
+int main() {
+	TimeSheet p("Pope", 10);
+
+	p.AddTime(4);
+	p.AddTime(7);
+	p.AddTime(6);
+	p.AddTime(5);
+	p.AddTime(1);
+	p.AddTime(2);
+
+	cout << p.GetStandardDeviation(); // 2.11476 ¹ÝÈ¯
+
+	return 0;
 }
