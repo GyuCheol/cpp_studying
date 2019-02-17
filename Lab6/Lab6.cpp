c1977694af9b1be2cc616a500602b53f44160b94
rbcjf0219 <rbcjf0219@gmail.com>
2019-02-18 04:15:09 +0900

    실습6

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -1,35 +1,116 @@
 #include "Lab6.h"
+#include <map>
 
 namespace lab6
 {
 	int Sum(const std::vector<int>& v)
 	{
-		return 0;
+		int total = 0;
+		int size = v.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			total += v[i];
+		}
+		
+		return total;
 	}
 
 	int Min(const std::vector<int>& v)
 	{
-		return 0;
+		int min = INT32_MAX;
+		int size = v.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			if (min > v[i])
+			{
+				min = v[i];
+			}
+		}
+
+		return min;
 	}
 
 	int Max(const std::vector<int>& v)
 	{
-		return 0;
+		int max = INT32_MIN;
+		int size = v.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			if (max < v[i])
+			{
+				max = v[i];
+			}
+		}
+
+		return max;
 	}
 
 	float Average(const std::vector<int>& v)
 	{
-		return 0.0f;
+		int size = v.size();
+
+		if (size == 0)
+		{
+			return 0.f;
+		}
+		
+		return (float)Sum(v) / size;
 	}
 
 	int NumberWithMaxOccurrence(const std::vector<int>& v)
 	{
-		return 0;
+		int size = v.size();
+		int max = INT32_MIN;
+		int maxKey;
+		std::map<int, int> map;
+
+		if (size == 0)
+		{
+			return 0;
+		}
+
+		for (int i = 0; i < size; i++)
+		{
+			if (map.find(v[i]) == map.end())
+			{
+				map[v[i]] = 1;
+			}
+			else
+			{
+				map[v[i]]++;
+			}
+
+			if (max < map[v[i]])
+			{
+				maxKey = v[i];
+				max = map[v[i]];
+			}
+		}
+
+		return maxKey;
 	}
 
 	void SortDescending(std::vector<int>& v)
 	{
+		int size = v.size();
 
+		// selection sort
+		for (int i = 0; i < size - 1; i++)
+		{
+			for (int j = i; j < size; j++)
+			{
+				if (v[i] < v[j])
+				{
+					int tmp = v[i];
+
+					v[i] = v[j];
+					v[j] = tmp;
+				}
+			}
+		}
 	}
 
 }
--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -1,19 +1,33 @@
-#include <iostream>
-#include <vector>
+#include <cassert>
+#include "Lab6.h"
 
 int main()
 {
-	std::vector<int> scores;
+	std::vector<int> v;
+	v.push_back(4);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(12);
+	v.push_back(7);
+	v.push_back(11);
 
-	scores.reserve(2);
+	int sum = lab6::Sum(v);
+	int max = lab6::Max(v);
+	int min = lab6::Min(v);
+	float average = lab6::Average(v);
+	int numWithMaxOccurence = lab6::NumberWithMaxOccurrence(v);
+	lab6::SortDescending(v);
 
-	scores.push_back(30);
-	scores.push_back(50);
+	assert(sum == 41);
+	assert(max == 12);
+	assert(min == 3);
+	assert(average == 6.83333349f);
+	assert(numWithMaxOccurence == 4);
 
-	scores.pop_back();
+	for (int i = 0; i < (int)v.size() - 1; ++i)
+	{
+		assert(v[i] >= v[i + 1]);
+	}
 
-	std::cout << scores.capacity();
-	std::cout << std::endl;
-	std::cout << scores.size();
-
+	return 0;
 }
