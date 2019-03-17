7d0849c652084744b7c088f0ce7202c5a92da230
rbcjf0219 <rbcjf0219@gmail.com>
2019-03-17 21:23:39 +0900

    실습8

---
+++ Lab8/FixedBoolVector.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#include <cstdint>
+
+namespace lab8 {
+
+	template <size_t N>
+	class FixedVector<bool, N> {
+	public:
+		FixedVector();
+		~FixedVector();
+
+		bool Add(bool value);
+		bool Remove(bool find);
+		const bool Get(unsigned int index);
+		int GetIndex(bool find);
+		size_t GetSize();
+		size_t GetCapacity();
+
+		bool operator[](int index);
+
+	private:
+		enum { BIT_SIZE = sizeof(uint32_t) * 8};
+
+		uint32_t* mValues;
+		size_t mArraySize;
+		size_t mSize = 0;
+	};
+
+	template <size_t N>
+	bool FixedVector<bool, N>::Add(bool value)
+	{
+		
+		if (mSize == N) {
+			return false;
+		}
+		
+		int pos = mSize / BIT_SIZE;
+		int id = mSize % BIT_SIZE;
+
+		if (value == true)
+		{
+			mValues[pos] |= (1 << id);
+		}
+		else
+		{
+			mValues[pos] &= ~(1 << id);
+		}
+
+		mSize++;
+
+		return true;
+	}
+
+	template <size_t N>
+	bool FixedVector<bool, N>::operator[](int index)
+	{
+		return Get(index);
+	}
+
+	template <size_t N>
+	bool FixedVector<bool, N>::Remove(bool value)
+	{
+		int index = GetIndex(value);
+
+		if (index == -1)
+		{
+			return false;
+		}
+
+		int pos = index / BIT_SIZE;
+		int id = index % BIT_SIZE;
+
+		uint32_t left = mValues[pos] % (1 << id);
+		uint32_t right = mValues[pos] >> (id + 1);
+
+		mValues[pos] = left + (right << id);
+		if (pos < (mArraySize - 1))
+		{
+			mValues[pos] |= (mValues[pos + 1] % 2) << (BIT_SIZE - 1);
+		}
+
+		for (size_t i = pos + 1; i < mArraySize; i++)
+		{
+			mValues[i] = mValues[i] >> 1;
+			if (i < (mArraySize - 1))
+			{
+				mValues[i] |= (mValues[i + 1] % 2) << (BIT_SIZE - 1);
+			}
+		}
+
+		mSize--;
+
+		return true;
+	}
+
+	template <size_t N>
+	const bool FixedVector<bool, N>::Get(unsigned int index)
+	{
+		int pos = index / BIT_SIZE;
+		int id = index % BIT_SIZE;
+
+		return mValues[pos] & (1 << id);
+	}
+
+	template <size_t N>
+	int FixedVector<bool, N>::GetIndex(bool find)
+	{
+		for (size_t i = 0; i < mSize; i++)
+		{
+			if (find == Get(i)) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	template <size_t N>
+	size_t FixedVector<bool, N>::GetSize()
+	{
+		return mSize;
+	}
+
+	template <size_t N>
+	size_t FixedVector<bool, N>::GetCapacity()
+	{
+		return N;
+	}
+
+	template <size_t N>
+	FixedVector<bool, N>::FixedVector()
+	{
+		mArraySize = N / BIT_SIZE;
+
+		if (N % BIT_SIZE != 0) {
+			mArraySize++;
+		}
+
+		mValues = new uint32_t[mArraySize]{ 0, };
+	}
+
+	template <size_t N>
+	FixedVector<bool, N>::~FixedVector()
+	{
+		delete[] mValues;
+	}
+
+}
+
---
+++ Lab8/FixedVector.h
@@ -0,0 +1,96 @@
+#pragma once
+
+namespace lab8 {
+
+	template <typename T, size_t N>
+	class FixedVector {
+	public:
+		bool Add(T value);
+		bool Remove(T find);
+		const T& Get(unsigned int index);
+		int GetIndex(T find);
+		size_t GetSize();
+		size_t GetCapacity();
+
+		T& operator[](int index);
+
+	private:
+		T mValues[N];
+		size_t mSIze = 0;
+
+	};
+
+	template<typename T, size_t N>
+	bool FixedVector<T, N>::Add(T value)
+	{
+		if (mSIze == N) {
+			return false;
+		}
+
+		mValues[mSIze] = value;
+
+		mSIze++;
+
+		return true;
+	}
+
+	template<typename T, size_t N>
+	bool FixedVector<T, N>::Remove(T find)
+	{
+		int id = GetIndex(find);
+
+		if (id == -1)
+		{
+			return false;
+		}
+
+		for (size_t i = id; i < mSIze - 1; i++)
+		{
+			mValues[i] = mValues[i+1];
+		}
+
+		mSIze--;
+
+		return true;
+	}
+
+	template<typename T, size_t N>
+	const T& FixedVector<T, N>::Get(unsigned int index)
+	{
+		return mValues[index];
+	}
+
+	template<typename T, size_t N>
+	int FixedVector<T, N>::GetIndex(T find)
+	{
+		for (size_t i = 0; i < mSIze; i++)
+		{
+			if (find == mValues[i])
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	template<typename T, size_t N>
+	size_t FixedVector<T, N>::GetSize()
+	{
+		return mSIze;
+	}
+
+	template<typename T, size_t N>
+	size_t FixedVector<T, N>::GetCapacity()
+	{
+		return N;
+	}
+
+	template<typename T, size_t N>
+	T& FixedVector<T, N>::operator[](int index)
+	{
+		return mValues[index];
+	}
+
+}
+
