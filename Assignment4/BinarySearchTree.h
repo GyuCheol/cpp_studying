8a2ce73c341bc948b22db8599b935299e8c706ca
rbcjf0219 <rbcjf0219@gmail.com>
2019-04-08 00:34:44 +0900

    과제4 제추루우울

---
+++ Assignment4/BinarySearchTree.h
@@ -0,0 +1,249 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <stack>
+
+#include "TreeNode.h"
+
+namespace assignment4
+{
+	template<typename T>
+	class TreeNode;
+
+	template<typename T>
+	class BinarySearchTree final
+	{
+	public:
+		void Insert(std::unique_ptr<T> data);
+		bool Search(const T& data);
+		bool Delete(const T& data);
+		const std::weak_ptr<TreeNode<T>> GetRootNode() const;
+
+		static std::vector<T> TraverseInOrder(const std::shared_ptr<TreeNode<T>> startNode);
+
+	private:
+		std::shared_ptr<TreeNode<T>> mRoot;
+		static void search(std::vector<T>& vector, const std::shared_ptr<TreeNode<T>>& node);
+
+	};
+
+	template<typename T>
+	void BinarySearchTree<T>::Insert(std::unique_ptr<T> data)
+	{
+		
+		if (mRoot == nullptr)
+		{
+			mRoot = std::make_shared<TreeNode<T>>(std::move(data));
+			return;
+		}
+
+		std::shared_ptr<TreeNode<T>> tmp = mRoot;
+
+		while (true)
+		{
+			if (*tmp->Data >= *data)
+			{
+				if (tmp->Left == nullptr)
+				{
+					tmp->Left = std::make_shared<TreeNode<T>>(tmp, std::move(data));
+					return;
+				}
+				else
+				{
+					tmp = tmp->Left;
+				}
+			}
+			else
+			{
+				if (tmp->Right == nullptr)
+				{
+					tmp->Right = std::make_shared<TreeNode<T>>(tmp, std::move(data));
+					return;
+				}
+				else
+				{
+					tmp = tmp->Right;
+				}
+			}
+		}
+		
+	}
+
+	template<typename T>
+	const std::weak_ptr<TreeNode<T>> BinarySearchTree<T>::GetRootNode() const
+	{
+		return mRoot;
+	}
+
+	template<typename T>
+	bool BinarySearchTree<T>::Search(const T& data)
+	{
+		std::shared_ptr<TreeNode<T>> tmp = mRoot;
+
+		while (true)
+		{
+			if (tmp == nullptr)
+			{
+				return false;
+			}
+
+			if (*tmp->Data == data)
+			{
+				return true;
+			}
+
+			tmp = (*tmp->Data >= data) ? tmp->Left : tmp->Right;
+		}
+	}
+
+	template<typename T>
+	bool BinarySearchTree<T>::Delete(const T& data)
+	{
+		std::shared_ptr<TreeNode<T>> tmp = mRoot;
+
+		while (true)
+		{
+
+			if (tmp == nullptr)
+			{
+				return false;
+			}
+
+			if (*tmp->Data == data)
+			{
+				break;
+			}
+
+			tmp = (*tmp->Data >= data) ? tmp->Left : tmp->Right;
+		}
+
+		std::shared_ptr<TreeNode<T>> parent = tmp->Parent.lock();
+
+		// Leaf Node일때
+		if (tmp->Left == nullptr && tmp->Right == nullptr)
+		{
+			if (tmp == mRoot)
+			{
+				mRoot = nullptr;
+			}
+			else
+			{
+				if (parent->Left == tmp)
+				{
+					parent->Left = nullptr;
+				}
+				else
+				{
+					parent->Right = nullptr;
+				}
+			}
+		}
+		// 자식을 2개 가지는 Node
+		else if (tmp->Left != nullptr && tmp->Right != nullptr)
+		{
+			if (tmp == mRoot)
+			{
+				mRoot = tmp->Left;
+			}
+			else
+			{
+				if (parent->Left == tmp)
+				{
+					parent->Left = tmp->Left;
+				}
+				else
+				{
+					parent->Right = tmp->Left;
+				}
+
+				tmp->Left->Parent = parent;
+			}
+
+			std::shared_ptr<TreeNode<T>> tmp2 = tmp->Left;
+
+			while (true)
+			{
+				if (tmp2->Right == nullptr)
+				{
+					tmp2->Right = tmp->Right;
+					// ?
+					break;
+				}
+
+				tmp2 = tmp2->Right;
+			}
+		}
+		// 왼쪽 노드만 있는 경우
+		else if (tmp->Left != nullptr)
+		{
+			if (tmp == mRoot)
+			{
+				mRoot = tmp->Left;
+			}
+			else
+			{
+				if (parent->Left == tmp)
+				{
+					parent->Left = tmp->Left;
+				}
+				else
+				{
+					parent->Right = tmp->Left;
+				}
+				tmp->Left->Parent = parent;
+			}
+		}
+		// 오른쪽 노드만 있는 경우
+		else if (tmp->Right != nullptr)
+		{
+			if (tmp == mRoot)
+			{
+				mRoot = tmp->Right;
+			}
+			else
+			{
+				if (parent->Left == tmp)
+				{
+					parent->Left = tmp->Right;
+				}
+				else
+				{
+					parent->Right = tmp->Right;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	template<typename T>
+	std::vector<T> BinarySearchTree<T>::TraverseInOrder(const std::shared_ptr<TreeNode<T>> startNode)
+	{
+		std::vector<T> v;
+
+		if (startNode != nullptr)
+		{
+			search(v, startNode);
+		}
+
+		return v;
+	}
+
+	template<typename T>
+	void BinarySearchTree<T>::search(std::vector<T>& vector, const std::shared_ptr<TreeNode<T>>& node)
+	{
+		if (node->Left != nullptr)
+		{
+			search(vector, node->Left);
+		}
+
+		vector.push_back(*node->Data);
+
+		if (node->Right != nullptr)
+		{
+			search(vector, node->Right);
+		}
+	}
+
+}
--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -1,4 +1,107 @@
-int main()
+#include <memory>
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+#include "BinarySearchTree.h"
+
+using namespace assignment4;
+
+void test1()
+{
+	BinarySearchTree<int> tree;
+
+	tree.Insert(std::make_unique<int>(3));
+	tree.Insert(std::make_unique<int>(1));
+	tree.Insert(std::make_unique<int>(4));
+
+	assert(tree.Search(1) == true);
+	assert(tree.Search(2) == false);
+	assert(tree.Search(3) == true);
+	assert(tree.Search(4) == true);
+	assert(tree.Search(5) == false);
+
+	assert(tree.Delete(3) == true);
+
+	assert(tree.GetRootNode().lock()->Parent.expired());
+
+	auto v = tree.TraverseInOrder(tree.GetRootNode().lock());
+
+	assert(v[0] == 1);
+	assert(v[1] == 4);
+
+	tree.Insert(std::make_unique<int>(5));
+	tree.Insert(std::make_unique<int>(100));
+
+	v = tree.TraverseInOrder(tree.GetRootNode().lock());
+
+	assert(v.size() == 4);
+	assert(v[0] == 1);
+	assert(v[1] == 4);
+	assert(v[2] == 5);
+	assert(v[3] == 100);
+}
+
+void test3()
 {
 
+}
+
+void test2()
+{
+	BinarySearchTree<int> tree;
+
+	tree.Insert(std::make_unique<int>(10));
+	tree.Insert(std::make_unique<int>(15));
+	tree.Insert(std::make_unique<int>(5));
+	tree.Insert(std::make_unique<int>(4));
+	tree.Insert(std::make_unique<int>(19));
+	tree.Insert(std::make_unique<int>(20));
+	tree.Insert(std::make_unique<int>(17));
+	tree.Insert(std::make_unique<int>(12));
+
+	std::vector<int> v = tree.TraverseInOrder(tree.GetRootNode().lock());
+
+	assert(v.size() == 8);
+	assert(v[0] == 4);
+	assert(v[1] == 5);
+	assert(v[2] == 10);
+	assert(v[3] == 12);
+	assert(v[4] == 15);
+	assert(v[5] == 17);
+	assert(v[6] == 19);
+	assert(v[7] == 20);
+
+	bool bSearched = tree.Search(15);
+	assert(bSearched);
+
+	bool bDeleted = tree.Delete(6);
+	assert(!bDeleted);
+
+	bDeleted = tree.Delete(100);
+	assert(!bDeleted);
+
+	bDeleted = tree.Delete(15);
+	assert(bDeleted);
+
+	v = tree.TraverseInOrder(tree.GetRootNode().lock());
+
+	assert(v.size() == 7);
+	assert(v[0] == 4);
+	assert(v[1] == 5);
+	assert(v[2] == 10);
+	assert(v[3] == 12);
+	assert(v[4] == 17);
+	assert(v[5] == 19);
+	assert(v[6] == 20);
+
+	bSearched = tree.Search(15);
+	assert(!bSearched);
+}
+
+int main()
+{
+	test1();
+	test2();
+	test3();
 }
